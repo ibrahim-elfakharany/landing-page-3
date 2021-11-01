c688153ad7f9463566e90c3996fe9c7c7f97b943
ibrahimadel-web <hemaelfakhrany003@gmail.com>
2021-11-01 23:45:48 +0200

    progress and fix navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,25 @@
 $(function () {
-  $(".toggler").on("click", function () {
-    $(".navigator").html($(".links").html());
-    $(".navigator").slideToggle();
+  let navigator = $(".navigator");
+  let toggler = $(".toggler");
+  
+  toggler.on("click", function () {
+    navigator.html($(".links").html());
+    navigator.slideToggle();
+  });
+  $(window).on("resize", function () {
+    if (toggler.css("display") == "none") {
+      navigator.hide();
+    }
   });
 });
+
+let prog = document.querySelector(".the-progress");
+let spans = document.querySelectorAll(".the-progress span");
+
+window.onscroll = function () {
+  if (window.scrollY >= prog.offsetTop - 300) {
+    spans.forEach((span) => {
+      span.style.cssText = `opacity:1;color:#fff;width:${span.dataset.width}`;
+    });
+  }
+};
